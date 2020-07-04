03297610e4309db0a2358d3de83f1847007ca5df
Tommy <tommycrabtree@outlook.com>
2020-07-04 02:47:37 -0400

    End of Section 3

--- framing.api/Controllers/ValuesController.cs
+++ framing.api/Controllers/ValuesController.cs
@@ -2,11 +2,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using framing.api.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace framing.api.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
--- framing.api/Data/DataContext.cs
+++ framing.api/Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace framing.api.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
